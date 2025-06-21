3a0cc1300385b334bfed74ba71d53c662f167ba9
James Hayton <jameshayton@James-MacBook-Pro.local>
2025-06-21 12:57:45 -0500

    Use single prepend wrapper for callbacks (Rails-7/Ruby-3.2 safe)

--- lib/couchrest/model/callbacks.rb
+++ lib/couchrest/model/callbacks.rb
@@ -1,28 +1,46 @@
 # encoding: utf-8
-
 module CouchRest #:nodoc:
   module Model #:nodoc:
-
     module Callbacks
       extend ActiveSupport::Concern
 
-      CALLBACKS = [
-        :before_validation, :after_validation,
-        :after_initialize,
-        :before_create, :around_create, :after_create,
-        :before_destroy, :around_destroy, :after_destroy,
-        :before_save, :around_save, :after_save,
-        :before_update, :around_update, :after_update,
+      CALLBACKS = %i[
+        before_validation after_validation
+        after_initialize
+        before_create around_create after_create
+        before_destroy around_destroy after_destroy
+        before_save   around_save   after_save
+        before_update around_update after_update
       ]
 
+      # ------------------------------------------------------------------
+      # Declare the events – this part was already in the file
+      # ------------------------------------------------------------------
       included do
-        extend ActiveModel::Callbacks
+        extend  ActiveModel::Callbacks
         include ActiveModel::Validations::Callbacks
 
         define_model_callbacks :initialize, :only => :after
         define_model_callbacks :create, :destroy, :save, :update
       end
-    end
 
+      # ------------------------------------------------------------------
+      # NEW: single prepend-based wrapper (Rails-5+ idiom)
+      # ------------------------------------------------------------------
+      module PrependWrapper
+        def save(*a, **k, &b)    = run_callbacks(:save)    { super }
+        def create(*a, **k, &b)  = run_callbacks(:create)  { super }
+        def update(*a, **k, &b)  = run_callbacks(:update)  { super }
+        def destroy(*a, **k, &b) = run_callbacks(:destroy) { super }
+      end
+
+      # ActiveSupport::Concern already defines .included;
+      # we extend it and then prepend our wrapper exactly once.
+      def self.included(base)
+        super
+        base.prepend PrependWrapper
+      end
+    end
   end
 end
+

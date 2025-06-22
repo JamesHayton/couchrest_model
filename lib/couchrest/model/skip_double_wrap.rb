3ebca2d4d5b2d2a16c76083d75b7675fd06103a9
James Hayton <jameshayton@James-MacBook-Pro.local>
2025-06-21 19:26:14 -0500

    x

--- lib/couchrest/model/skip_double_wrap.rb
+++ lib/couchrest/model/skip_double_wrap.rb
@@ -5,16 +5,17 @@
 module CouchRest
   module Model
     module SkipDoubleWrap
-      def set_callback(name, *filters, &block)
+      def set_callback(name, *filter_list, &block)
         @_cr_seen_events ||= {}
+
         if @_cr_seen_events[name]
-          _insert_callbacks(name, filters, &block)  # just add filters
+          # Wrapper already exists → just append the filters
+          send(:_insert_callbacks, name, filter_list, &block)
         else
-          @_cr_seen_events[name] = true            # first time → wrap
-          super
+          @_cr_seen_events[name] = true
+          super                           # first time → build wrapper
         end
       end
     end
   end
 end
-
